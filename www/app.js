c2fde2a0a38729ef08df3ad0f9eab55df6330b96
BaoShyang <sjsyphonetography@gmail.com>
2019-06-19 12:06:26 +0800

    'update'

--- www/app.js
+++ www/app.js
@@ -5,24 +5,17 @@ window.fn.open = function() {
   menu.open();
 };
 
-// window.fn.load = function(page) {
-//   var content = document.getElementById('content');
-//   var menu = document.getElementById('menu');
-//   content.load(page).then(menu.close.bind(menu));
-// };
-
 document.addEventListener('init', function(event) {
   var page = event.target;
 
-  if (page.id === 'settings.html') {
-    // ons.notification.toast('settings.html...', { timeout: 2000 });
+  if (page.id === 'home.html') {
     page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
   }
 });
 
 window.fn.load = function(page, mytitle) {
   var navigator = document.getElementById('myNavigator');
-  var menu = document.getElementById('home.html');
+  var menu = document.getElementById('menu');
   data = { data: { title: mytitle }, animation: 'slide' };
   navigator.pushPage(page, data).then(menu.close.bind(menu));
 };
